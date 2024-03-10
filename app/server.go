766fb3da58163607d48581fbcccd1d4062a6364e
Mohammed785 <mohammednady128@gmail.com>
2024-03-10 19:06:49 +0200

    1st stage: accept connection

--- app/server.go
+++ app/server.go
@@ -6,6 +6,20 @@ import (
 	"os"
 )
 
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	var data []byte
+	_, err := conn.Read(data)
+	if err != nil {
+		fmt.Printf("[Error:%s] Couldn't Read data %v\n", conn.RemoteAddr(), err)
+	}
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error Replaying: ",err.Error())
+		os.Exit(1)
+	}
+}
+
 func main() {
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -13,9 +27,10 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	connection, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	handleConnection(connection)
 }
