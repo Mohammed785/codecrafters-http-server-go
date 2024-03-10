223d98e4c784e029b01159fcb19aa0d8df2a74b1
Mohammed785 <mohammednady128@gmail.com>
2024-03-10 23:45:54 +0200

    respond with content

--- app/server.go
+++ app/server.go
@@ -4,25 +4,107 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
+var statusMessages = map[int]string{200: "OK", 201: "No Content", 404: "Not Found", 400: "Bad Request"}
+
+type Request struct {
+	Method  string
+	Path    string
+	Version string
+	Headers map[string]string
+	// Body
+}
+
+type Response struct {
+	Status  int
+	Headers map[string]string
+	Body    string
+}
+
+func (r Response) joinHeaders() string {
+	str := ""
+	for k, v := range r.Headers {
+		str += fmt.Sprintf("%v: %v\r\n\r\n", k, v)
+	}
+	return str
+}
+
+func (r Response) Bytes() []byte {
+	return []byte(
+		fmt.Sprintf("HTTP/1.1 %v %v\r\n\r\n%v%v", r.Status, statusMessages[r.Status], 
+		r.joinHeaders(), r.Body,
+	))
+}
+
+type HTTPError struct {
+	Message       string
+	StatusMessage string
+	Status        int
+}
+
+func (err HTTPError) Error() string {
+	return fmt.Sprintf("HTTP/1.1 %v %v\r\n\r\n", err.Status, err.StatusMessage)
+}
+
+func NewResponse(status int, content string, headers map[string]string) Response {
+	resp := Response{Status: status, Headers: headers}
+	if content != "" {
+		resp.Headers["Content-Length"] = strconv.Itoa(len(content))
+	}
+	resp.Body = content
+	return resp
+}
+
+func ParseRequest(conn net.Conn) (*Request, error) {
+	req := &Request{Headers: map[string]string{}}
+	scanner := bufio.NewScanner(conn)
+	readStart := true
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+		// read start-line
+		if readStart {
+			requestLine := strings.Split(line, " ")
+			if len(requestLine) != 3 {
+				return nil, HTTPError{Status: http.StatusBadRequest, StatusMessage: "Bad Request", Message: fmt.Sprintf("Invalid start line %s", line)}
+			}
+			req.Method = requestLine[0]
+			req.Path = requestLine[1]
+			req.Version = requestLine[2]
+			readStart = false
+		} else {
+			// read headers
+			header := strings.Split(line, ": ")
+			if len(header) != 2 {
+				return nil, HTTPError{Status: http.StatusBadRequest, StatusMessage: "Bad Request", Message: fmt.Sprintf("Invalid Header %s", header[0])}
+			}
+			req.Headers[header[0]] = header[1]
+		}
+	}
+	return req, nil
+}
+
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	sc := bufio.NewScanner(conn)
-	sc.Split(bufio.ScanLines)
-	if !sc.Scan() {
-		fmt.Printf("[Error:%s] Couldn't Read data\n", conn.RemoteAddr())
+	req, err := ParseRequest(conn)
+	if err != nil {
+		conn.Write([]byte(err.Error()))
 		os.Exit(1)
 	}
-	requestLine := strings.Split(sc.Text(), " ")
-	if requestLine[1] == "/" {
+	if req.Path == "/" {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if idx := strings.Index(req.Path, "echo/"); idx != -1 {
+		conn.Write(NewResponse(200, req.Path[idx+5:], map[string]string{"Content-Type": "text/plain"}).Bytes())
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
-
 }
 
 func main() {
