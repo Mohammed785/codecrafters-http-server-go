71a5fd3842b393c58b58c996f5a64b148e11dbc8
Mohammed785 <mohammednady128@gmail.com>
2024-03-10 22:02:41 +0200

    3rd stage respond with 404

--- app/server.go
+++ app/server.go
@@ -1,23 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
-	var data []byte
-	_, err := conn.Read(data)
-	if err != nil {
-		fmt.Printf("[Error:%s] Couldn't Read data %v\n", conn.RemoteAddr(), err)
-	}
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	if err != nil {
-		fmt.Println("Error Replaying: ",err.Error())
+	sc := bufio.NewScanner(conn)
+	sc.Split(bufio.ScanLines)
+	if !sc.Scan() {
+		fmt.Printf("[Error:%s] Couldn't Read data\n", conn.RemoteAddr())
 		os.Exit(1)
 	}
+	requestLine := strings.Split(sc.Text(), " ")
+	if requestLine[1] == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+
 }
 
 func main() {
@@ -27,6 +32,7 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	fmt.Println("Server Started")
 	connection, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
